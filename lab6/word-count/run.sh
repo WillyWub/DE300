985a22bb1c9c676575a53eef1db4fee1f28c7112
Ubuntu <ubuntu@ip-10-192-10-221.us-east-2.compute.internal>
2024-05-15 00:16:10 +0000

    lab66

--- lab6/word-count/run.sh
+++ lab6/word-count/run.sh
@@ -1,12 +1,4 @@
-
-
-
-
-
-
-
-
-docker run -v /Users/robertsu/Documents/TA/DE300/lab_doc/lab6/word-count:/tmp/wc-demo -it \
+docker run -v /home/ubuntu/DE300/lab6/word-count:/tmp/wc-demo -it \
 	   -p 8888:8888 \
            --name wc-container \
 	   pyspark-image
